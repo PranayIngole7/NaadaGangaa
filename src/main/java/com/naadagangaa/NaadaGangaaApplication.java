ea8ef20d444930015c4af988ff1265167eb4ae59
PranayIngole1 <pranayingole1@gmail.com>
2026-09-10 14:37:47 +0530

    config: add database application settings and main application class

---
+++ src/main/java/com/naadagangaa/NaadaGangaaApplication.java
@@ -0,0 +1,12 @@
+package com.naadagangaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaadaGangaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaadaGangaaApplication.class, args);
+    }
+}
